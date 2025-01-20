7c3a429614d41b7d79491ba17fd225878ac8d8af
Sanjay Khatri <www.khatrisanjay@gmail.com>
2025-01-20 10:50:30 +0545

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideToastr} from 'ngx-toastr';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
     provideAnimations(),
-    provideToastr()
+    provideToastr(),
+    provideStoreDevtools(),
   ]
 };
