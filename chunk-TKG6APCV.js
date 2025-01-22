0998d1f4cb1204adf4874c51914c53f21e229bc9
Sanjay Khatri <www.khatrisanjay@gmail.com>
2025-01-22 21:44:28 +0545

    Auto-generated commit

--- chunk-TKG6APCV.js
+++
@@ -1,1 +0,0 @@
-import{$ as a,g as o}from"./chunk-777UGNZZ.js";var i=class e{loadingSubject=new o(!1);loading$=this.loadingSubject.asObservable();show(){this.loadingSubject.next(!0)}hide(){this.loadingSubject.next(!1)}toggle(t){this.loadingSubject.next(t)}static \u0275fac=function(n){return new(n||e)};static \u0275prov=a({token:e,factory:e.\u0275fac,providedIn:"root"})};export{i as a};
