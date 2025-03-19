50c6606e60f1ea581070521616545ce86edc5fc2
Sanjay Khatri <khatrisanjay804@gmail.com>
2025-03-19 11:33:48 +0545

    environment modifu

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,15 +3,25 @@ export const environment = {
   LOCAL_STORAGE_NAME: 'Sanjay Khatri',
   PRODUCT_NAME: 'Sanju Portfolio',
   PRODUCT_SLOGAN: 'Be Yourself',
-  baseApiUrl: 'https://personal-website-209f0-default-rtdb.firebaseio.com/',
+  baseApiUrl: '',
+  openRouter: {
+    URL: '',
+    KEY: '',
+    MODEL: '',
+  },
+  huggingFace: {
+    URL: '',
+    KEY: '',
+    MODEL: '',
+  },
   firebase: {
-    apiKey: "AIzaSyC93GF3Tj0gOcFLr9hXeZ-tc0Sb48mePGU",
-    authDomain: "personal-website-209f0.firebaseapp.com",
-    databaseURL: "https://personal-website-209f0-default-rtdb.firebaseio.com",
-    projectId: "personal-website-209f0",
-    storageBucket: "personal-website-209f0.firebasestorage.app",
-    messagingSenderId: "480898171820",
-    appId: "1:480898171820:web:92ac5c4f8428a4723e3009",
-    measurementId: "G-NYJJ1XLNT2"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
